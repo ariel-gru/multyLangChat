ed968d1f4198fcc8ae374e19b42c60ba27993b8e
ariel-gru <arielg140907@gmail.com>
2025-03-16 13:21:42 +0200

    fixed the char 0 issue

--- app/client.py
+++ app/client.py
@@ -70,7 +70,7 @@ class client:
     def snd_msg(self,message,username):
             msg=message
             print(msg)
-            msg=username+":"+chr(0)+msg
+            msg=username+":"+msg
             encrypted_msg = self.cipher.aes_encrypt(msg.encode())
             self.client_socket.send(encrypted_msg)
 
